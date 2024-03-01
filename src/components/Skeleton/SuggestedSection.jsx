25de1ecb4c737a1bb5444834c3a13df10d385754
msuryasyahruli <msuryasyahruli12@gmail.com>
2024-03-01 13:41:40 +0700

    add skeleton

---
+++ src/components/Skeleton/SuggestedSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SuggestedSection = () => {
+    return (
+        <>
+            <div role='status' class="max-w-sm animate-pulse px-2">
+                <div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-24 mb-2"></div>
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-28"></div>
+            </div>
+        </>
+    )
+}
+
+export default SuggestedSection
